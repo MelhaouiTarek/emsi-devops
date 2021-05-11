95a8d655c9b3112896ab36b60fe3ac083bd2a7bf
xune <29429662+skri56@users.noreply.github.com>
2021-05-11 23:15:25 +0100

    altered 0-subs.py

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -1,24 +1,17 @@
-
 #!/usr/bin/python3
 """
 Getting number of subscribers for a subreddit
 """
 import requests
-
-
 def number_of_subscribers(subreddit):
     """
-        function returns number of subscribers
-        and returns 0 for invalid subreddit
+    function returns number of subscribers or 0 if invalid subreddit
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    # setting user agent
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-
+    #user agent
+    headers = {'User-Agent': 'Test'}
     response = requests.get(url, headers=headers).json()
-    subs = response.get('data', {}).get('subscribers')
+    subs = response.get('data',{}).get('subscribers')
     if not subs:
         return 0
     return subs
